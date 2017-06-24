72b88d330ea853595c34cf27503868cc937e5578
kert <kertsad@gmail.com>
2017-06-24 20:13:55 +0300

    иНТЕРХРЕНАЦИЯ

--- snake/snake/Point.cs
+++ snake/snake/Point.cs
@@ -8,14 +8,23 @@ namespace snake
 {
     class Point
     {
+      
         public int x;
         public int y;
         public char sym;
+
+        public Point(int xx, int yy, char symsym)
+        {
+            x = xx;
+            y = yy;
+            sym = symsym;
+        }
 
         public void Draw_point()
         {
             Console.SetCursorPosition(x, y);
             Console.Write(sym);
         }
+       
     }
 }
--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -10,16 +10,16 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point();
-            p1.x = 10;
+            Point p1 = new Point(10, 20, '^');
+            /*p1.x = 10;
             p1.y = 10;
-            p1.sym = '^';
+            p1.sym = '^';*/
             p1.Draw_point();
 
-            Point p2 = new Point();
-            p2.x = 24;
+            Point p2 = new Point(20, 10, '^');
+            /*p2.x = 24;
             p2.y = 40;
-            p2.sym = '^';
+            p2.sym = '^';*/
             p2.Draw_point();
 
             Console.ReadLine();
