1d77282a37923c0a342e3292e6983dfd509031db
kert <kertsad@gmail.com>
2017-06-26 20:19:27 +0300

    lists added

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -11,16 +11,32 @@ namespace snake
         static void Main(string[] args)
         {
             Point p1 = new Point(10, 20, '^');
-            /*p1.x = 10;
-            p1.y = 10;
-            p1.sym = '^';*/
-            p1.Draw_point();
-
             Point p2 = new Point(20, 10, '^');
-            /*p2.x = 24;
-            p2.y = 40;
-            p2.sym = '^';*/
-            p2.Draw_point();
+            Point p3 = new Point(7, 22, '#');
+            Point p4 = new Point(35, 5, '*');
+            Point p5 = new Point(56, 8, '%');
+
+            List<Point> pointlist = new List<Point>();
+            pointlist.Add(p1);
+            pointlist.Add(p2);
+            pointlist.Add(p3);
+            pointlist.Add(p4);
+            pointlist.Add(p5);
+
+            foreach (Point i in pointlist)
+            {
+                i.Draw_point();
+            }
+
+            List<char> charlist = new List<char>();
+            charlist.Add('%');
+            charlist.Add('c');
+            charlist.Add('[');
+
+            foreach(char i in charlist)
+            {
+                Console.Write(i);
+            }
 
             Console.ReadLine();
         }
